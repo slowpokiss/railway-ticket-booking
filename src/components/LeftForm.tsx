2cbc871b32b86bee3e06d4157e183fcc591b9a2e
Michel <hltvexe@mail.ru>
2024-07-03 18:53:14 +0300

    updated filters

--- src/components/LeftForm.tsx
+++ src/components/LeftForm.tsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import "../css/Switcher.css";
+
+interface filterTimeProps {
+  direction?: string;
+  title?: string
+}
 
 
+function FilterTime({ direction, title }: filterTimeProps) {
+  const [extensionState, setExtensionState] = useState(false);
+
+  const onExtendClick = (ev: React.MouseEvent<HTMLImageElement>) => {
+    setExtensionState(!extensionState);
+  };
+
+  return (
+    <>
+      <div className="border-b border-white">
+        <div className="flex p-5  justify-between">
+          <div className="flex gap-2">
+            <img src={`../vecs/${direction}_icon.svg`} alt={direction} />
+              <p className="font-semibold">{title}</p>
+          </div>
+          <img
+            onClick={onExtendClick}
+            className={`cursor-pointer`}
+            src={`../vecs/${extensionState ? "collapse" : "extend"}.svg`}
+            alt="collapse"
+          />
+        </div>
+
+        <div className={`${extensionState ? "block" : "hidden"}`}>
+          <div className="p-5 text-[16px]">
+            <p>Время отбытия</p>
+            <input type="text" className="input-template text-[14px] p-2" />
+          </div>
+          <div className="p-5 text-[16px]">
+            <p className="text-end">Время прибытия</p>
+            <input type="text" className="input-template text-[14px] p-2" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default function LeftForm() {
   return (
@@ -10,68 +54,74 @@ export default function LeftForm() {
             <div>Дата поездки</div>
             <input
               type="text"
-              className="input-template py-1 px-3 bg-[url('../../public/vecs/calendar_logo.svg')]"
+              className="input-template py-1 px-3 bg-[url('../../vecs/calendar_logo.svg')]"
             />
           </div>
           <div className="">
             <div>Дата возвращения</div>
             <input
               type="text"
-              className="input-template py-1 px-3 bg-[url('../../public/vecs/calendar_logo.svg')]"
+              className="input-template py-1 px-3 bg-[url('../../vecs/calendar_logo.svg')]"
             />
           </div>
         </div>
-        <ul className="flex justify-start flex-col text-[15px] gap-5 px-11 py-5 border-b border-white">
-          <li className="flex gap-5 ">
+        <ul className="flex justify-start flex-col text-[15px] gap-5 px-[17%] py-5 border-b border-white">
+          <li className="flex gap-7 justify-between">
             <img src="../vecs/kype.svg" alt="kype" />
-            Купе
+            <p className="mr-auto">Купе</p>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round"></span>
+            </label>
           </li>
-          <li className="flex gap-5">
+          <li className="flex gap-7 justify-between">
             <img src="../vecs/plazc.svg" alt="plazc" />
-            Плацкарт
+            <p className="mr-auto">Плацкарт</p>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round"></span>
+            </label>
           </li>
-          <li className="flex gap-5">
+          <li className="flex gap-7 justify-between">
             <img src="../vecs/sitting.svg" alt="sitting" />
-            Сидячий
+            <p className="mr-auto">Сидячий</p>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round"></span>
+            </label>
           </li>
-          <li className="flex gap-5">
+          <li className="flex gap-7 justify-between">
             <img src="../vecs/luxury.svg" alt="luxury" />
-            Люкс
+            <p className="mr-auto">Люкс</p>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round"></span>
+            </label>
           </li>
-          <li className="flex gap-5">
+          <li className="flex gap-7 justify-between">
             <img src="../vecs/wifi.svg" alt="wifi" />
-            Wi-Fi
+            <p className="mr-auto">Wi-Fi</p>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round"></span>
+            </label>
           </li>
-          <li className="flex gap-5">
+          <li className="flex gap-7 justify-between">
             <img src="../vecs/express.svg" alt="express" />
-            Экспресс
+            <p className="mr-auto">Экспресс</p>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round"></span>
+            </label>
           </li>
         </ul>
-        <div className="flex flex-col border-b border-white">
+        <div className="flex flex-col border-b p-5 border-white">
           <div className="">Стоимость</div>
+          <input type="text" className="input-template text-[14px] p-2" />
         </div>
-        <div className="flex p-5 text-[18px] justify-between border-b border-white">
-          <div className="flex gap-2">
-            <img src="../vecs/forward_icon.svg" alt="forward" />
-            <p className="font-medium">Туда</p>
-          </div>
-          <img
-            className="cursor-pointer"
-            src="../vecs/collapse.svg"
-            alt="collapse"
-          />
-        </div>
-        <div className="flex p-5 text-[18px] justify-between">
-          <div className="flex gap-2">
-            <img src="../vecs/backward_icon.svg" alt="backward" />
-            <p className="font-medium">Обратно</p>
-          </div>
-          <img
-            className="cursor-pointer"
-            src="../vecs/collapse.svg"
-            alt="collapse"
-          />
-        </div>
+
+        <FilterTime direction="forward" title="Туда" />
+        <FilterTime direction="backward" title="Обратно" />
       </div>
     </>
   );
